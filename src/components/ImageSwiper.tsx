676e2bcdb150528669ad82d20dc7fe150fc316c1
matteoFagundez <matteoFagundez@gmail.com>
2023-05-26 19:52:49 -0300

    Imagenes del banner actualizadas

--- src/components/ImageSwiper.tsx
+++ src/components/ImageSwiper.tsx
@@ -4,19 +4,17 @@ import Swiper from 'react-native-swiper';
 
 const ImageSwiper = () => {
   const productImages = [
-    '../../assets/1.png',
-    'https://th.bing.com/th/id/R.837a8250e601ea514120fc8bf0632f2a?rik=35U3W3o4rb8hXQ&riu=http%3a%2f%2fimg2.rtve.es%2fimagenes%2fmayoria-alimentos-sanos-no-pueden-probar-cientificamente-sean%2f1212587585377.jpg&ehk=XNBCrtnwmGkGyHUIIIOsruM2AMxJZyCjkNQ1KOMXl70%3d&risl=&pid=ImgRaw&r=0',
-    'https://via.placeholder.com/300',
-    'https://via.placeholder.com/300',
-    'https://via.placeholder.com/300',
-    'https://via.placeholder.com/300',
-    'https://via.placeholder.com/300',
+    require('../../assets/1.png'), 
+    require('../../assets/2.png'),
+    require('../../assets/3.png'),
+    require('../../assets/4.png'),
+    require('../../assets/5.png'),
   ];
 
   const renderImages = () => {
     return productImages.map((image, index) => (
       <View key={index} style={styles.slide}>
-        <Image source={{ uri: image }} style={styles.image} />
+        <Image source={image} style={styles.image} /> 
       </View>
     ));
   };
@@ -26,7 +24,7 @@ const ImageSwiper = () => {
       <Swiper
         style={styles.wrapper}
         autoplay
-        autoplayTimeout={5}
+        autoplayTimeout={3}
         showsPagination={true}
       >
         {renderImages()}
@@ -37,7 +35,7 @@ const ImageSwiper = () => {
 
 const styles = StyleSheet.create({
   container: {
-    height: 200, // Establece una altura fija para el componente ImageSwiper
+    height: 200,
     paddingTop: 20,
   },
   wrapper: {},
